49319c59baf799f4c61a043cafaa186c7d92c3e6
irispon <47470214+irispon@users.noreply.github.com>
2020-08-10 21:44:40 +0900

    [리소스] 리소스 업로드

--- XcomCopyProject/Assets/Utill/UI/Loading/SceneLoader.cs
+++ XcomCopyProject/Assets/Utill/UI/Loading/SceneLoader.cs
@@ -7,63 +7,64 @@ using UnityEngine;
 using UnityEngine.Networking;
 using WebSocketSharp.Server;
 
-public class SceneLoader : MonoBehaviour
+namespace Assets.Utill.UI.Loading
 {
-    [SerializeField]
-    private string scene;
-    [SerializeField]
-    private List<Loader> loaders;
-    [SerializeField]
-    bool isLocal;
-    // Start is called before the first frame update
-    void Start()
+    public class SceneLoader : MonoBehaviour
     {
-
-
-        LocalCheck();
-        List<ILoader> iloaders = new List<ILoader>(loaders);
-        LoadingManager.LoadScene(scene, loaders: iloaders);
-
-    }
-
-    void LocalCheck()
-    {
-
-
-        UnityWebRequest uwr = UnityWebRequest.Get(ServerHelper.SERVERPATH);
-
-        uwr.SendWebRequest();
-
-        while (!uwr.isDone)
+        [SerializeField]
+        private string scene;
+        [SerializeField]
+        private List<Loader> loaders;
+        [SerializeField]
+        bool isLocal;
+        // Start is called before the first frame update
+        void Start()
         {
-            new WaitForSeconds(0.1f);
-            //나중에 변경해야될 코드.(로그인이 안되는 경우)
+
+
+            LocalCheck();
+            List<ILoader> iloaders = new List<ILoader>(loaders);
+            LoadingManager.LoadScene(scene, loaders: iloaders);
+
         }
 
-
-
-        if (uwr.isNetworkError)
+        void LocalCheck()
         {
-    
-            ServerHelper.SERVERPATH = "http://192.168.219.126:4444";
-            Debug.Log("Error While Sending: " + ServerHelper.SERVERPATH);
-        }
-        else
-        {
-            Debug.Log("Received: " + uwr.downloadHandler.text);
 
-            if (uwr.downloadHandler.text.Equals("true"))
+
+            UnityWebRequest uwr = UnityWebRequest.Get(ServerHelper.SERVERPATH);
+
+            uwr.SendWebRequest();
+
+            while (!uwr.isDone)
             {
+                new WaitForSeconds(0.1f);
+                //나중에 변경해야될 코드.(로그인이 안되는 경우)
+            }
+
+
+
+            if (uwr.isNetworkError)
+            {
+
+                ServerHelper.SERVERPATH = "http://192.168.219.126:4444";
+                Debug.Log("Error While Sending: " + ServerHelper.SERVERPATH);
+            }
+            else
+            {
+                Debug.Log("Received: " + uwr.downloadHandler.text);
+
+                if (uwr.downloadHandler.text.Equals("true"))
+                {
+
+                }
+
+
 
             }
 
 
 
         }
-
-
-
     }
 }
-
-
